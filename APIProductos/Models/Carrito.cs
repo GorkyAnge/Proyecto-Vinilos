b46b037e9747dd5ce1edf5b9ce35fabfaef47c00
MateoRosero <mateorgafjo@gmail.com>
2023-10-29 18:47:10 -0500

    Agregando clases y controladores "Carrito"

---
+++ APIProductos/Models/Carrito.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIProductos.Models
+{
+    public class Carrito
+    {
+        [Key]
+        public int IdCarrito { get; set; }
+        [Key]
+        public int IdUsuario { get; set; }
+        public List<Producto> ListaProductos { get; set; }
+        public float Total { get; set; }
+    }
+}
---
+++ EjercicioEnClase2/Models/Carrito.cs
@@ -0,0 +1,10 @@
+﻿namespace EjercicioEnClase2.Models
+{
+    public class Carrito
+    {
+        public int IdCarrito { get; set; }
+        public int IdUsuario { get; set; }
+        public List<Producto> ListaProductos { get; set; }
+        public float Total { get; set; }
+    }
+}
